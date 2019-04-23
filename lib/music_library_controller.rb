02c45af41592e1ef25e431e42bb4a7631b489c34
David Cantoran <davidcantoran36@gmail.com>
2019-04-23 02:26:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,7 +23,7 @@ class MusicLibraryController
   end 
   
   def list_songs 
-    Song.all.sort_by(&:name).each_with_index(1) do |song, idx|
+    Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
       puts "#{idx} - #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end 
   end 
