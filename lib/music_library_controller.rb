26679022239337982111ca1c641dfc8c3696f235
David Cantoran <davidcantoran36@gmail.com>
2019-04-23 02:29:37 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,4 +28,10 @@ class MusicLibraryController
     end 
   end 
   
+  def list_artists 
+    Artist.all.sort_by(&:name).each.with_index(1) do |artist, idx|
+      puts "#{idx}. #{artist.name}"
+    end 
+  end 
+  
 end 
